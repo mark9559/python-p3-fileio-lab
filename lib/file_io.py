2c38202535e6f477e051c012cf344a6fc58e05cc
mark9559 <mwangimark57@gmail.com>
2023-11-30 01:45:23 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    try:
+        with open(f"{file_name}.txt", "a") as file:
+            file.write(append_content)
+    except FileNotFoundError:
+        print("File not found")
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found"
+
